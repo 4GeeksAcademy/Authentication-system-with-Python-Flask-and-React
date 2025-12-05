40cf59732c345d1a48b2d820d82caa8215f11a78
JenifferGenoves <62884668+JenifferGenoves@users.noreply.github.com>
2025-12-05 19:02:31 +0000

    Add pages and routes

--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -10,19 +10,25 @@ import { Home } from "./pages/Home";
 import { Single } from "./pages/Single";
 import { Demo } from "./pages/Demo";
 
+// Importaciones nuevas 
+import { Signup } from "./pages/Signup"
+import { Login } from "./pages/Login"
+import { Private } from "./pages/Private"
+
 export const router = createBrowserRouter(
     createRoutesFromElements(
-    // CreateRoutesFromElements function allows you to build route elements declaratively.
-    // Create your routes here, if you want to keep the Navbar and Footer in all views, add your new routes inside the containing Route.
-    // Root, on the contrary, create a sister Route, if you have doubts, try it!
-    // Note: keep in mind that errorElement will be the default page when you don't get a route, customize that page to make your project more attractive.
-    // Note: The child paths of the Layout element replace the Outlet component with the elements contained in the "element" attribute of these child paths.
 
       // Root Route: All navigation will start from here.
       <Route path="/" element={<Layout />} errorElement={<h1>Not found!</h1>} >
 
         {/* Nested Routes: Defines sub-routes within the BaseHome component. */}
         <Route path= "/" element={<Home />} />
+
+        {/* Nuevas Rutas requeeridas */}
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element ={<Login />} />
+        <Route path="/private" element={<Private />} />
+
         <Route path="/single/:theId" element={ <Single />} />  {/* Dynamic route for single items */}
         <Route path="/demo" element={<Demo />} />
       </Route>
